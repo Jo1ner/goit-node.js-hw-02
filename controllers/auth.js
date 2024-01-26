de35f34210a4cbcbc4418b02a3da7c9ead54fc39
Semeniuk Dmytro <semdimas@gmail.com>
2024-01-26 12:20:44 +0200

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,7 +53,7 @@ const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.json({ message: "Logout success" });
+  res.status(204).json({ message: "Logout success" });
 };
 
 module.exports = {
